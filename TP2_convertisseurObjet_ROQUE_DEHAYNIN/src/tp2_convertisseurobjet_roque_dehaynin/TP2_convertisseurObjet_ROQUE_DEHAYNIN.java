b55271bc3b1ba9f0fd2e9432e4298a8eb750789b
Jade <Jade@192.168.115.172>
2021-11-12 14:11:02 +0100

    fin exo2 tp2

--- TP2_convertisseurObjet_ROQUE_DEHAYNIN/src/tp2_convertisseurobjet_roque_dehaynin/TP2_convertisseurObjet_ROQUE_DEHAYNIN.java
+++ TP2_convertisseurObjet_ROQUE_DEHAYNIN/src/tp2_convertisseurobjet_roque_dehaynin/TP2_convertisseurObjet_ROQUE_DEHAYNIN.java
@@ -4,6 +4,8 @@
  */
 package tp2_convertisseurobjet_roque_dehaynin;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Jade
@@ -15,6 +17,76 @@ public class TP2_convertisseurObjet_ROQUE_DEHAYNIN {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Convertisseur temperature = new Convertisseur (); 
+        
+        double result=temperature.CelciusVersKelvin(45);
+        
+       
+        System.out.println(result);
+        
+        double result2=temperature.KelvinVersCelcius(100);
+        
+        System.out.println(result2);
+        
+        double result3=temperature.KelvinVersFarenheit(-12);
+        
+        System.out.println(result3);
+        
+        System.out.println(temperature);
+        
+        
+        double temp1; 
+        Scanner sc; 
+      sc=new Scanner (System.in); 
+        int c;
+      
+      
+      System.out.println("Bonjour,saisissez une valeur ");
+      
+    temp1 = sc.nextDouble(); 
+    System.out.println("Saisissez la conversion que vous souhaiter effectuer ?\n 1) De Celcius vers Kelvin \n 2) De Kelvin vers Celcius\n 3) De Farenheit Vers Celcius \n 4) De Celcius Vers Farenheit\n 5)De Kelvin Vers Farenheit \n 6) De Farenheit vers Kelvin");
+    c = sc.nextInt();
+    
+      switch(c){
+              
+              case 1 :
+      
+        
+        System.out.println("La température est  "+temp1+"K");
+        break;
+              case 2:      
+        
+        temp1 = KelvinVersCelcius(temp1) ; 
+        System.out.println("La température est  "+temp1+"°C"); 
+        break;
+              case 3:
+       
+        temp1 = CelciusVersFarenheit(temp1) ; 
+        System.out.println("La température est  "+temp1+"F"); 
+        break;
+              case 4:
+    
+        temp1 = FarenheitVersCelcius(temp1) ; 
+        System.out.println("La température est  "+temp1+"°C"); 
+        break;
+        
+              case 5:
+        
+        temp1 = KelvinVersFarenheit(temp1) ; 
+        System.out.println("La température est  "+temp1+"F"); 
+        break;
+        
+              case 6:
+     
+        temp1 = FarenheitVersKelvin(temp1) ; 
+        System.out.println("La température est  "+temp1+"K"); 
+        break;
+        
+        default:
+            
+    }
     }
     
-}
+    }
+    
+
