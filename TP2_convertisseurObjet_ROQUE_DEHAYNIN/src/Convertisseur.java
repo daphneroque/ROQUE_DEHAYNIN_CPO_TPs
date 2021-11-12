2ec36cebf84378a941cb2dc6056ada22132e0e86
Jade <Jade@192.168.115.172>
2021-11-12 14:11:44 +0100

    fin exo2 tp2 class convertisseur

--- TP2_convertisseurObjet_ROQUE_DEHAYNIN/src/Convertisseur.java
+++ TP2_convertisseurObjet_ROQUE_DEHAYNIN/src/Convertisseur.java
@@ -1,10 +1,10 @@
 
 
-/*
+/*package 
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
+package tp2_convertisseurobjet_roque_dehaynin;
 import java.util.Scanner;
 /**
  *
