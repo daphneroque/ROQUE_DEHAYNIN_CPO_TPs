b1670cb6edf242bb909604d2098db8bad7807198
Jade <Jade@192.168.115.172>
2021-11-12 15:04:54 +0100

    fin exo 3 tp2

--- TP2_manip_ROQUE_DEHAYNIN/src/tp2_manip_roque_dehaynin/TP2_manip_ROQUE_DEHAYNIN.java
+++ TP2_manip_ROQUE_DEHAYNIN/src/tp2_manip_roque_dehaynin/TP2_manip_ROQUE_DEHAYNIN.java
@@ -15,6 +15,34 @@ public class TP2_manip_ROQUE_DEHAYNIN {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Tartiflette assiette1 = new Tartiflette(500) ;
+        Tartiflette assiette2 = new Tartiflette(600) ;
+        assiette2.nbCalories = 100;
+        Tartiflette assiette3 = assiette2 ;
+        
+        // il y a 2 new donc deux tartiflettes; L'ASSIETE 2 et l'assiete 3 sont la même tartiflette
+        System.out.println("nb de calories de Assiette 2 : " + assiette2.nbCalories) ;
+        System.out.println("nb de calories de Assiette 3 : " + assiette3.nbCalories) ;
+        System.out.println("nb decalories de Assiette 1: " + assiette1.nbCalories);
+        
+        assiette2 = assiette1;
+        assiette1 = assiette3;
+        
+        // les deux sont fausses : on ne peut pas entrer un objet dans une classe qui n'est pas la sienne
+        
+    Moussaka [] tableau = new Moussaka[10];
+    for (int i=0;i<tableau.length;i++){
+    tableau[i] = new Moussaka(100*(1+i));
+    if (i==tableau.length-1){
+            System.out.println(tableau[i]);   
+        }
+    else{
+            System.out.print(tableau[i]);   
+
     }
-    
+    }
+    }
 }
+
+
+    
