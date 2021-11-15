d55d9345996939cd89d2fd1f4b8c1dbfd6672ff7
daphn <daphn@192.168.114.203>
2021-11-15 11:45:41 +0100

    cellule grille

--- SP4_console_ROQUE_DEHAYNIN/src/sp4_console_roque_dehaynin/Grille.java
+++ SP4_console_ROQUE_DEHAYNIN/src/sp4_console_roque_dehaynin/Grille.java
@@ -9,5 +9,34 @@ package sp4_console_roque_dehaynin;
  * @author daphn
  */
 public class Grille {
+    Cellule[][] CellulesJeu =new Cellule [6][7];
     
+    public void Grille() {
+        for (int i=0; i<6; i++){
+            for (int j=0; j<7; j++){
+                CellulesJeu[i][j]=null;
+            }
+        }
+            
 }
+    public boolean ajouterJetonDansColonne(Jeton jet,int colonne){
+
+        for (int i=0; i<6; i++){
+            if (CellulesJeu[i][colonne].jetonCourant==null){
+                CellulesJeu[i][colonne].affecterJeton(jet);
+                return true;
+            }
+            
+            }
+        return false;   
+        }
+    
+        
+        
+    }
+   
+    
+    
+    
+    
+
