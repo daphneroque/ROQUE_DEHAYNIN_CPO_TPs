08c72717ddc2d0cd25ecc3a79a831a1f42c97226
Jade <Jade@192.168.115.172>
2021-11-30 19:11:45 +0100

    commentaire classe joueur

--- SP4_console_ROQUE_DEHAYNIN/src/sp4_console_roque_dehaynin/Joueur.java
+++ SP4_console_ROQUE_DEHAYNIN/src/sp4_console_roque_dehaynin/Joueur.java
@@ -10,7 +10,7 @@ import java.util.Random;
  *
  * @author daphn
  */
-public class Joueur {
+public class Joueur { //on crée nos attributs dont un tableau de 21 cases
 
     String Nom ;
     String Couleur;
@@ -18,18 +18,18 @@ public class Joueur {
     int nombreJetonsRestants;
     int  nombreDesintegrateurs;
 
-    public Joueur(String c) {
+    public Joueur(String c) { // on affecte le nom joueur avec son parametre
         Nom = c;
     }
 
-    public void affecterCouleur(String coul) {
-        Couleur = coul;
+    public void affecterCouleur(String coul) { //affecter la couleur en parametre au joueur
+        Couleur = coul; 
 
     }
 
-    public boolean ajouterJeton(Jeton pion) {
+    public boolean ajouterJeton(Jeton pion) { //on ajoute le jeton en parametre a la liste de jeton
         int i = 0;
-        while (i < ListeJetons.length) {
+        while (i < ListeJetons.length) { // on crée une boucle qui permet d'initialiser notre liste jeton
             if (ListeJetons[i] == null) {
                 ListeJetons[i] = pion;
                 nombreJetonsRestants++;
@@ -38,29 +38,29 @@ public class Joueur {
             if (ListeJetons[i] != null) {
                 i++;
             }
-            if (i == 21) {
+            if (i == 21) { // il y a max 21 jetons il peut pas en avoir plus 
                 return false;
 
             }
 
         }
-        return true;
+        return true; // cela permets de faire fonctionner notre methode
     }
     
     
-    public void obtenirDesintegrateur(){
+    public void obtenirDesintegrateur(){ //incrémente le nombre de désintégrateurs du joueur
         nombreDesintegrateurs = nombreDesintegrateurs +1;
    
     }
     
-    public boolean utiliserDesintegrateur(){
+    public boolean utiliserDesintegrateur(){ // decremente le nombre de desintegrateur 
         if (nombreDesintegrateurs>0){
             nombreDesintegrateurs=nombreDesintegrateurs-1;
-            return true;
+            return true;  // confirme l'tulisation du desintegrateur
    
         }
         else {
-            return false;
+            return false; // il ne reste plus de desintegrateur a utiliser
         }
 
     }
